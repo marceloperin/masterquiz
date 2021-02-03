ca1303364ad87901670e13e71f0c4a67139b3e18
Marcelo <marcelo.pborba@gmail.com>
2021-02-03 19:33:33 -0300

    Created quiz page

--- pages/quiz.js
+++ pages/quiz.js
@@ -1,13 +1,105 @@
 import React from 'react';
+import styled from 'styled-components';
+import Head from 'next/head';
+import { useRouter } from 'next/router';
 
-export default function QuizPages() {
+import Widget from '../src/components/Widget';
+import QuizBackground from '../src/components/QuizBackground';
+import QuizContainer from '../src/components/QuizContainer';
 
-  const params = new URLSearchParams(window.location.search);
-  const name = params.get('name');
+import Button from '../src/components/Button';
+import db from '../db.json';
+
+function LoadingWidget() {
+  return (
+    <Widget>
+      <Widget.Header>
+        Carregando...
+      </Widget.Header>
+
+      <Widget.Content>
+        asdjf
+      </Widget.Content>
+    </Widget>
+  );
+}
+
+function QuestionWidget({ question, totalQuestions, questionIndex }) {
+  const questionId = `question_${questionIndex}`;
 
   return (
-    <div>
-      <p style="color: #000">Hello ${name}</p>
-    </div>
+    <Widget>
+      <Widget.Header>
+        <h1>
+          Pergunta {questionIndex + 1} de {totalQuestions}
+        </h1>
+      </Widget.Header>
+
+      <img
+        alt="Descricao"
+        style={{
+          width: '100%',
+          height: '150px',
+          objectFit: 'cover',
+        }}
+        src={question.image}
+      />
+
+      <Widget.Content>
+        <h2>
+          {question.title}
+        </h2>
+        <p>
+          {question.description}
+        </p>
+
+        <form>
+          {question.alternatives.map((alternative, alternativeIndex) => {
+            const alternativeId = `alternative_${alternativeIndex}`
+            return (
+              <Widget.Topic 
+                as="label"
+                htmlFor={alternativeId}
+              >
+                <input 
+                  style={{display: 'none'}}
+                  id={alternativeId}
+                  name={questionId}
+                  type="radio"
+                />
+                {alternative}
+              </Widget.Topic>
+            )
+          })}
+        </form>
+
+        {/* <pre>
+          {JSON.stringify(question, null, 4)}
+        </pre> */}
+       
+        <Button>
+          Confirmar
+        </Button>
+      </Widget.Content>
+    </Widget>
+  );
+}
+
+export default function QuizPage() {
+  const totalQuestions = db.questions.length;
+  const questionIndex = 0;
+  const question = db.questions[questionIndex];
+
+  return (
+    <QuizBackground backgroundImage={db.bg}>
+      <QuizContainer>
+        <QuestionWidget 
+          question={question}
+          questionIndex={questionIndex}
+          totalQuestions={totalQuestions}
+        />
+        <LoadingWidget />
+      </QuizContainer>
+    </QuizBackground>
   );
 }
